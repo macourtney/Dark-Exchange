39e1e3dd98247f383a27741353a68e2d91eb8473
Matt Courtney <macourtney@gmail.com>
2011-06-21 10:01:40 -0400

    Added support for all currencies.

--- src/darkexchange/model/payment_type.clj
+++ src/darkexchange/model/payment_type.clj
@@ -2,11 +2,23 @@
   (:require [darkexchange.model.currency :as currency]
             [darkexchange.model.terms :as terms]))
 
-(def mailable-currencies ["USD" "AUD" "CAD" "CHF" "EUR" "GBP" ])
+(def checkable-currencies ["AED" "AFN" "ALL" "AMD" "ANG" "AOA" "ARS" "AUD" "AWG" "AZN" "BAM" "BBD" "BDT" "BGN" "BHD"
+                           "BIF" "BMD" "BND" "BOB" "BRL" "BSD" "BTN" "BWP" "BYR" "BZD" "CAD" "CDF" "CHF" "CLP" "CNY"
+                           "COP" "CRC" "CUP" "CVE" "CZK" "DJF" "DKK" "DOP" "DZD" "EGP" "ERN" "ETB" "EUR" "FJD" "FKP"
+                           "GBP" "GEL" "GGP" "GHS" "GIP" "GMD" "GNF" "GTQ" "GYD" "HKD" "HNL" "HRK" "HTG" "HUF" "IDR"
+                           "ILS" "IMP" "INR" "IQD" "IRR" "ISK" "JEP" "JMD" "JOD" "JPY" "KES" "KGS" "KHR" "KMF" "KPW"
+                           "KRW" "KWD" "KYD" "KZT" "LAK" "LBP" "LKR" "LRD" "LSL" "LTL" "LVL" "LYD" "MAD" "MDL" "MGA"
+                           "MKD" "MMK" "MNT" "MOP" "MRO" "MUR" "MVR" "MWK" "MXN" "MYR" "MZN" "NAD" "NGN" "NIO" "NOK"
+                           "NPR" "NZD" "OMR" "PAB" "PEN" "PGK" "PHP" "PKR" "PLN" "PYG" "QAR" "RON" "RSD" "RUB" "RWF"
+                           "SAR" "SBD" "SCR" "SDG" "SEK" "SGD" "SHP" "SLL" "SOS" "SPL" "SRD" "STD" "SVC" "SYP" "SZL"
+                           "THB" "TJS" "TMM" "TND" "TOP" "TRY" "TTD" "TVD" "TWD" "TZS" "UAH" "UGX" "USD" "UYU" "UZS"
+                           "VEF" "VND" "VUV" "WST" "XAF" "XCD" "XOF" "XPF" "YER" "ZAR" "ZMK" "ZWD"])
+
+(def mailable-currencies (concat checkable-currencies ["XAG" "XAU" "XPD" "XPT"]))
 
 (def payment-types [{ :code "BITCOIN" :name (terms/bitcoin-transfer) :currency-types ["BITCOIN"] }
                     { :code "CAM" :name (terms/cash-by-mail) :currency-types mailable-currencies }
-                    { :code "CHM" :name (terms/check-or-money-order-by-mail) :currency-types mailable-currencies }
+                    { :code "CHM" :name (terms/check-or-money-order-by-mail) :currency-types checkable-currencies }
                     { :code "DWOLLA" :name (terms/dwolla) :currency-types ["USD"] }])
 
 (def payment-type-map (reduce #(assoc %1 (:code %2) %2) {} payment-types))
--- src/darkexchange/view/offer/widgets.clj
+++ src/darkexchange/view/offer/widgets.clj
@@ -5,7 +5,7 @@
   (seesaw-core/text :id id :preferred-size [100 :by 25]))
 
 (defn create-currency-combobox [id]
-  (seesaw-core/combobox :id id :preferred-size [120 :by 25]))
+  (seesaw-core/combobox :id id :preferred-size [270 :by 25]))
 
 (defn create-payment-type-combobox [id]
   (seesaw-core/combobox :id id :preferred-size [180 :by 25]))
