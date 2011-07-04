4e09a79e12b9d6be53328245ca51efe60d80d03c
Matt Courtney <macourtney@gmail.com>
2011-07-04 16:20:09 -0400

    Updated the drift version to 1.2.1.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
                  [clojure-tools "1.0.0"]
                  [com.h2database/h2 "1.2.137"]
                  [commons-codec/commons-codec "1.5"]
-                 [drift "1.2.1-SNAPSHOT"]
+                 [drift "1.2.1"]
                  [log4j/log4j "1.2.16"]
                  [org.clojars.macourtney/clj-record "1.0.1"]
                  [org.clojars.macourtney/i2p "0.8.5-0"]
