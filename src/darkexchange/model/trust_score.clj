f474c99f1da14507d9a75fefce12454df4e1aa08
Matt Courtney <macourtney@gmail.com>
2011-08-18 12:30:54 -0400

    Updated the trust score model to not create dumplicate scores between identities.

--- src/darkexchange/model/trust_score.clj
+++ src/darkexchange/model/trust_score.clj
@@ -7,5 +7,17 @@
   (:associations (belongs-to scorer :fk scorer_id :model identity)
                  (belongs-to target :fk target_id :model identity)))
 
-(defn add-trust-score [target-identity basic-score]
-  (insert { :scorer_id (:id (identity/current-user-identity)) :target_id (:id target-identity) :basic basic-score }))
+(defn find-trust-score
+  ([target-identity] (find-trust-score (identity/current-user-identity) target-identity))
+  ([scorer-identity target-identity]
+    (find-record { :scorer_id (:id scorer-identity) :target_id (:id target-identity) })))
+
+(defn add-trust-score
+  ([target-identity basic-score] (add-trust-score (identity/current-user-identity) target-identity basic-score))
+  ([scorer-identity target-identity basic-score]
+    (if-let [trust-score-id (:id (find-trust-score scorer-identity target-identity))]
+      (do
+        (update { :id trust-score-id :basic basic-score })
+        trust-score-id)
+      (insert { :scorer_id (:id scorer-identity) :target_id (:id target-identity) :basic basic-score }))))
+
--- test/test/darkexchange/model/trust_score.clj
+++ test/test/darkexchange/model/trust_score.clj
@@ -17,4 +17,22 @@
     (is (= basic-trust-score (:basic trust-score)))
     (is (= (:id (identity-model/current-user-identity)) (:scorer_id trust-score)))
     (is (= (:id target-identity) (:target_id trust-score)))
+    (let [basic-trust-score2 -0.5
+          trust-score-id2 (add-trust-score target-identity basic-trust-score2)
+          trust-score2 (when trust-score-id2 (get-record trust-score-id2))]
+      (is trust-score-id2)
+      (is (= trust-score-id trust-score-id2))
+      (is (= basic-trust-score2 (:basic trust-score2)))
+      (when (not (= trust-score-id trust-score-id2))
+        (destroy-record trust-score2)))
+    (destroy-record trust-score))
+  (let [basic-trust-score -0.5
+        scorer-identity (identity-model/get-record 3)
+        target-identity (identity-model/get-record 1)
+        trust-score-id (add-trust-score scorer-identity target-identity basic-trust-score)
+        trust-score (when trust-score-id (get-record trust-score-id))]
+    (is trust-score-id)
+    (is (= basic-trust-score (:basic trust-score)))
+    (is (= (:id scorer-identity) (:scorer_id trust-score)))
+    (is (= (:id target-identity) (:target_id trust-score)))
     (destroy-record trust-score)))
